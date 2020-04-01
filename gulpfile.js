70a937a9c21428dc701bd013207b0ba5b710528d
rezaarkan <rezaarkan@gmail.com>
2020-04-01 22:22:47 +0200

    Initialize workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,50 @@
-var gulp = require('gulp'), 
-	sass = require('gulp-ruby-sass'), 
-	notify = require('gulp-notify'), 
-	rename = require('gulp-rename'), 
-	concat = require('gulp-concat'), 
-	uglify = require('gulp-uglify'), 
-	bower = require('gulp-bower'); 
-var lib = require('bower-files')(); 
-var config = { 
-	sassPath: './src/sass', 
-	bowerDir: './bower_components' 
-} 
-gulp.task('bower', function() { 
-	return bower() 
-		.pipe(gulp.dest(config.bowerDir)); 
-}); 
-gulp.task('icons', function() { 
-	return gulp.src([config.bowerDir + '/fontawesome/fonts/**.*', 
-		config.bowerDir + '/bootstrap-sass/assets/fonts/bootstrap/**.*']) 
-		.pipe(gulp.dest('./app/fonts')); 
-}); 
-gulp.task('js-vendor', function() { 
-	gulp.src(lib.ext('js').files) 
-		.pipe(concat('vendor.min.js')) 
-		.pipe(uglify()) 
-		.pipe(gulp.dest('./app/js')); 
-});  
-gulp.task('css', function() { 
-	return sass(config.sassPath + '/main.scss', {loadPath: [ 
-			'./src/sass', 
-			config.bowerDir + '/bootstrap-sass/assets/stylesheets', 
-			config.bowerDir + '/fontawesome/scss' 
-		], style: 'compressed',}) 
-		.pipe(rename('style.min.css')) 
-		.pipe(gulp.dest('./app/css')); 
-}); 
-gulp.task('watch', function() { 
-	gulp.watch(config.sassPath + '/**/*.scss',['css']); 
-}); 
-gulp.task('default', ['bower', 'icons', 'js-vendor', 'css']); 
+const gulp = require('gulp'),
+  $ = require('gulp-load-plugins')();
+
+var assetsSrcPath = '_assets/',
+    assetsDestPath = '_site/dist/assets',
+    cssSrcPath = '_sass/',
+    cssSrcName = 'main.scss',
+    cssDestPath = '_site/dist',
+    jsSrcPath = '_scripts/**/*.js',
+    jsDestPath = '_site/dist/',
+    jsDestName = 'script.js';
+
+const assets = function(){
+  return gulp.src(assetsSrcPath + '**')
+    .pipe(gulp.dest(assetsDestPath));
+}
+
+const css = function(){
+  return gulp.src(cssSrcPath + cssSrcName)
+    .pipe($.sourcemaps.init())
+    .pipe($.sass().on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      cascade: false
+    }))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest(cssDestPath));
+}
+
+const js = function(){
+  return gulp.src(jsSrcPath)
+    .pipe($.concat(jsDestName))
+    .pipe(gulp.dest(jsDestPath))
+}
+
+const jsbuild = function(){
+  return gulp.src(jsSrcPath)
+    .pipe($.concat(jsDestName))
+    .pipe($.uglify())
+    .pipe(gulp.dest(jsDestPath))
+}
+
+const watch = function(){
+  gulp.watch(cssSrcPath + '**/*.scss', gulp.parallel(css, js));
+}
+
+exports.assets = assets;
+exports.css = css;
+exports.js = js;
+exports.watch = watch;
+exports.build = gulp.parallel(assets, css, jsbuild);
